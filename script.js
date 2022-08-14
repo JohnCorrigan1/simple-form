11d5c4e4f0159744788eca21b254ef18a3294798
John Corrigan <jcorrigan322@gmail.com>
2022-08-14 16:57:36 -0400

    some comments before i forget

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//getting all elements
 const form = document.getElementById('form');
 const fname = document.getElementById('fname');
 const lname = document.getElementById('lname');
@@ -5,11 +6,16 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const passConfirm = document.getElementById('confirm');
 
+
+//event listener (on click submit button)
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    checkInputs();
+    checkInputs(); //check current inputs on click
 });
+
+
+
 
 function checkInputs() {
     const fnameValue = fname.value;
@@ -18,11 +24,11 @@ function checkInputs() {
     const passwordValue = password.value;
     const confirmValue = passConfirm.value;
 
-    
+    //call error function if input is left blank
     if(fnameValue == ''){
         setError(fname, "First Name is required..");
     }
-
+    //call error function if input is left blank
     if(lnameValue == ''){
         setError(lname, "Last Name is required...")
     }
@@ -30,6 +36,8 @@ function checkInputs() {
 
 }
 
+//error function sets small tag to right error message and 
+//changes input border
 function setError(input, message){
     const item = input.parentElement;
     const small = item.querySelector('small');
